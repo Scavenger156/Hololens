11b49b7e066b14abba6735d011a91852bfb98aae
scavenger156 <scavenger156@web.de>
2017-06-17 15:22:38 +0200

    Cubemanipulation

---
+++ TC_2_Cubemanipulation/Assets/MyScripts/CubeManipulation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class CubeManipulation : MonoBehaviour, IManipulationHandler, IInputClickHandler
+{
+    [Tooltip("Rotation speed")]
+    public float rotationSpeed = 10.0f;
+
+    
+    void Start () {
+		
+	} 
+	void Update () {
+		
+	}
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {       
+        float rotationFactor = eventData.CumulativeDelta.x * rotationSpeed;
+        transform.Rotate(new Vector3(0, -1 * rotationFactor,0));
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("OnInputClicked");
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+       
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+       
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+       
+    }
+}
